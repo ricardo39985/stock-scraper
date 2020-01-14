36b408be8ebe4611e6afbce525a87f6072de0b5b
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-14 06:29:17 +0000

    All pages are now parsed correctly

--- config/environment.rb
+++ config/environment.rb
@@ -7,17 +7,17 @@ class Scraper
   def initialize
     url = open(Url.main+"0")
     doc = Nokogiri::HTML(url)
-    listings = doc.css("tbody tr")
-    stocks = []
+
     page_counter = 0
-    per_page = listings.size.to_f
-    total_stocks = doc.css(".pager div")[0].text.split(" ")[-2].to_f
-    final_page = (total_stocks/per_page).round
-    while page_counter <=final_page
-      current_page = open(Url.main+"#{page_counter}")
+    stocks_per_page = doc.css("tbody tr").size.to_f
+    total_stocks = doc.css(".pager div")[0].text.split(" ")[-2].to_f #Extracted a total count of all stocks to float format
+    final_page = (total_stocks/stocks_per_page).round
+    stocks = []
+    while page_counter <=final_page #Goes through each page on the site
+      current_page = open(Url.main+"#{page_counter}") #changes the url on each iteration to up the site page count
       parsed = Nokogiri::HTML(current_page)
-      parsed.css("tbody tr").each do |stock|
-          stock_att = {
+      parsed.css("tbody tr").each do |stock| # Assigns the attribute for each listing on the page in a hash. Each pass of this
+        stock_att = {                      #will create one complete stock hash which will be added to the stocks array
           symbol: stock.css("td a")[0].text,
           company: stock.css(" a")[1].text,
           price: stock.css("td")[2].text,
@@ -27,8 +27,6 @@ class Scraper
           }
           stocks << stock_att
           page_counter+=1
-          binding.pry
-
         end
     end
 
