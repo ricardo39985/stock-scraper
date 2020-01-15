23c64697688af5312abe6e236090f3ddf35a8bdc
Ricardo Persaud <ricardo39985@gmail.com>
2020-01-15 00:38:56 +0000

    Scraper and Stock object creater working

--- config/environment.rb
+++ config/environment.rb
@@ -2,19 +2,28 @@ require "pry"
 require "nokogiri"
 require "open-uri"
 require_relative 'url.rb'
+require_relative 'stock.rb'
+
 
 class Scraper
   def initialize
     url = open(Url.main+"0")
-    doc = Nokogiri::HTML(url)
+    @doc = Nokogiri::HTML(url)
+    # counters
+    # import
+  end
+  def counters
+    @page_counter = 0
+    @stocks_per_page = @doc.css("tbody tr").size.to_f
+    @total_stocks = @doc.css(".pager div")[0].text.split(" ")[-2].to_f #Extracted a total count of all stocks to float format
+    @final_page = (@total_stocks/@stocks_per_page).round
+    @stocks = []
+  end
 
-    page_counter = 0
-    stocks_per_page = doc.css("tbody tr").size.to_f
-    total_stocks = doc.css(".pager div")[0].text.split(" ")[-2].to_f #Extracted a total count of all stocks to float format
-    final_page = (total_stocks/stocks_per_page).round
-    stocks = []
-    while page_counter <=final_page #Goes through each page on the site
-      current_page = open(Url.main+"#{page_counter}") #changes the url on each iteration to up the site page count
+  def import
+    counters
+    while @page_counter <= 1 #Goes through each page on the site
+      current_page = open(Url.main+"#{@page_counter}") #changes the url on each iteration to up the site page count
       parsed = Nokogiri::HTML(current_page)
       parsed.css("tbody tr").each do |stock| # Assigns the attribute for each listing on the page in a hash. Each pass of this
         stock_att = {                      #will create one complete stock hash which will be added to the stocks array
@@ -25,12 +34,11 @@ class Scraper
           percent_change: stock.css("td")[4].text,
           news: Url.news+stock.css("a")[3].attribute("href").value
           }
-          stocks << stock_att
-          page_counter+=1
+          @stocks << stock_att
         end
+      @page_counter+=1
     end
-
+    @stocks
   end
-
 end
-Scraper.new
+binding.pry
